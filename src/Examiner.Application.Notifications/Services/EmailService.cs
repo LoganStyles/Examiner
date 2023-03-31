512eaf3d976c3f54d72943652bd91814558f7345
LoganStyles <emmaist23@gmail.com>
2023-03-31 13:56:27 +0100

    feat(sending emails): added initial code & tests

--- src/Examiner.Application.Notifications/Services/EmailService.cs
+++ src/Examiner.Application.Notifications/Services/EmailService.cs
@@ -1,44 +1,59 @@
 using Examiner.Application.Notifications.Interfaces;
 using Examiner.Domain.Dtos;
+using Microsoft.Extensions.Logging;
 
 namespace Examiner.Application.Notifications.Services;
 
 public class EmailService : IEmailService
 {
     private readonly IVerificationService _verificationService;
+    private readonly ILogger<EmailService> _logger;
 
     private const string FAILED_VERIFICATION = "Email address is not verified";
     private const string EMAIL_SENDING_FAILED = "unable to send email";
     private const string EMAIL_SENDING_SUCCESSFUL = "Email sent successfully";
 
-    public EmailService(IVerificationService verificationService)
+    public EmailService(IVerificationService verificationService, ILogger<EmailService> logger)
     {
         _verificationService = verificationService;
+        _logger = logger;
     }
 
     /// <summary>
     /// Sends an email message to a user
     /// </summary>
-    /// <param name="email">The email of the user</param>
+    /// <param name="email">The email address to send to</param>
     /// <param name="message">The message to be sent</param>
     /// <returns>An GenericResponse object indicating the success or failure of an attempt to email a message</returns>
     public async Task<GenericResponse> SendMessage(string email, string message)
     {
 
         var resultResponse = GenericResponse.Result(false, FAILED_VERIFICATION);
+        if (string.IsNullOrWhiteSpace(email) || string.IsNullOrWhiteSpace(message))
+            return resultResponse;
 
-        var verificationResult = await _verificationService.IsVerified(email);
-        if (!verificationResult.Success)
+        try
         {
-            resultResponse.ResultMessage = verificationResult.ResultMessage;
+            var verificationResult = await _verificationService.IsVerified(email);
+            if (!verificationResult.Success)
+            {
+                resultResponse.ResultMessage = verificationResult.ResultMessage;
+                return resultResponse;
+            }
+
+            // send message to email, 
+            resultResponse.ResultMessage = EMAIL_SENDING_FAILED;
+            // resultResponse.Success = true;
+            /* we will store a record of an email sent */
             return resultResponse;
         }
-
-        // send message to email, 
-        resultResponse.ResultMessage = EMAIL_SENDING_FAILED;
-        /* we will store a record of an email sent */
-        return resultResponse;
-
+        catch (Exception ex)
+        {
+            // fetch inner exceptions if exist
+            _logger.LogError("Error sending message - ", ex.Message);
+            resultResponse.ResultMessage = ex.Message;
+            return resultResponse;
+        }
     }
 
 }
---
+++ tests/Examiner.Tests/Examiner.Application.Notifications/Services/EmailServiceTests.cs
@@ -0,0 +1,53 @@
+using Examiner.Application.Notifications.Interfaces;
+using Examiner.Application.Notifications.Services;
+using Examiner.Domain.Dtos;
+using Microsoft.Extensions.Logging.Abstractions;
+using Moq;
+
+namespace Examiner.Tests.Examiner.Application.Notifications.Services;
+
+public class EmailServiceTests
+{
+
+    private readonly Mock<IVerificationService> _verificationService;
+    private readonly EmailService _emailService;
+    private readonly NullLogger<EmailService> _logger;
+
+    private const string FAILED_VERIFICATION = "Email address is not verified";
+    private const string EMAIL_SENDING_FAILED = "unable to send email";
+    private const string EMAIL_SENDING_SUCCESSFUL = "Email sent successfully";
+    public EmailServiceTests()
+    {
+        _verificationService = new();
+        _logger = new();
+        _emailService = new(_verificationService.Object, _logger);
+    }
+
+    [Fact]
+    public async Task SendMessage_WithNullOrEmptyParams_Fails()
+    {
+
+        var result = await _emailService.SendMessage("", "");
+        Assert.False(result.Success);
+    }
+
+    [Fact]
+    public async Task SendMessage_WhenEmailIsNotVerified_Fails()
+    {
+        var verificationResponse = GenericResponse.Result(false, It.IsAny<string>());
+        _verificationService.Setup(v => v.IsVerified(It.IsAny<string>())).ReturnsAsync(verificationResponse);
+
+        var result = await _emailService.SendMessage("e@gmail.com", "adam");
+        Assert.False(result.Success);
+    }
+    
+    [Fact]
+    public async Task SendMessage_WhenEmailIsNotSent_Fails()
+    {
+        var verificationResponse = GenericResponse.Result(true, It.IsAny<string>());
+        _verificationService.Setup(v => v.IsVerified(It.IsAny<string>())).ReturnsAsync(verificationResponse);
+
+        var result = await _emailService.SendMessage("e@gmail.com", "adam");
+        Assert.False(result.Success);
+    }
+}
