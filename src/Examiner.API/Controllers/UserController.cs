fc69ee5d0ed800c8a0088ea0b612b4ab615ad05a
LoganStyles <emmaist23@gmail.com>
2023-04-25 15:39:08 +0100

    feat(user controller): updated responses to use proper responses

--- src/Examiner.API/Controllers/UserController.cs
+++ src/Examiner.API/Controllers/UserController.cs
@@ -40,14 +40,10 @@ public class UserController : ControllerBase
     [AllowAnonymous]
     [ProducesResponseType(StatusCodes.Status201Created)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    public async Task<ActionResult<GenericResponse>> RegisterAsync(
-        [FromBody] RegisterUserRequest request
-    )
+    public async Task<ActionResult<GenericResponse>> RegisterAsync([FromBody] RegisterUserRequest request)
     {
         if (!ModelState.IsValid)
-        {
             return BadRequest(ModelState);
-        }
 
         var response = GenericResponse.Result(false, $"{AppMessages.REGISTRATION} {AppMessages.FAILED}");
 
@@ -203,25 +199,6 @@ public class UserController : ControllerBase
         return Ok(result);
     }
 
-    // /// <summary>
-    // /// Fetches a user
-    // /// </summary>
-    // /// <param name="Id">The Id of the user to be fetched</param>
-    // /// <returns>Fetches a user or not found ActionResult</returns>
-    // [HttpGet("{Id:Guid}")]
-    // [ProducesResponseType(StatusCodes.Status200OK)]
-    // [ProducesResponseType(StatusCodes.Status401Unauthorized)]
-    // [ProducesResponseType(StatusCodes.Status404NotFound)]
-    // public async Task<ActionResult<GenericResponse>> GetByIdAsync(Guid Id)
-    // {
-
-    //     var result = await _userService.GetByIdAsync(Id);
-    //     if (result.Success)
-    //         return Ok(result);
-    //     else
-    //         return NotFound(result);
-    // }
-
     /// <summary>
     /// Fetches a user
     /// </summary>
@@ -238,7 +215,7 @@ public class UserController : ControllerBase
         if (user is not null)
             return Ok(ObjectMapper.Mapper.Map<UserResponse>(user));
         else
-            return NotFound($"{AppMessages.USER} {AppMessages.NOT_EXIST}");
+            return NotFound(GenericResponse.Result(false, $"{AppMessages.USER} {AppMessages.NOT_EXIST}"));
     }
 
 }
