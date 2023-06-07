a4baa682958d6f3b805cbb0e72999ddc54cc404e
ch9labs <ch9labsbybincom@gmail.com>
2023-06-07 08:40:44 +0100

    formatting

--- src/Examiner.API/Controllers/UserController.cs
+++ src/Examiner.API/Controllers/UserController.cs
@@ -43,6 +43,7 @@ public class UserController : ControllerBase
     //     }
     //     return allErrors;
     // }
+    
 
     /// <summary>
     /// Registers a user (without a role), creates user identity & default profile
